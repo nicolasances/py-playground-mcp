d80d008033138067dd94d35d9b782d320c231345
nicolasances <nicolas.matteazzi@gmail.com>
2026-01-22 14:36:48 +0100

    MCP Server construction from OpenAPI spec

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 from fastmcp import FastMCP, Context
+import httpx
 
 mcp = FastMCP("Toto Integration Example")
 
@@ -29,38 +30,46 @@ def get_bearer_token(ctx: Context):
     else:
         raise ValueError("Authorization header missing")
 
-@mcp.tool() 
-async def greet(name: str, context: Context) -> str:
-    """Greet a person by name."""
-    token = get_bearer_token(context)
+# @mcp.tool() 
+# async def greet(name: str, context: Context) -> str:
+#     """Greet a person by name."""
+#     token = get_bearer_token(context)
     
-    return f"Hello, {name}!"
+#     return f"Hello, {name}!"
 
-@mcp.tool()
-async def get_tome_topics(context: Context) -> list[Topic]:
-    """Get a list of topics from Tome API."""
-    token = get_bearer_token(context)
+# @mcp.tool()
+# async def get_tome_topics(context: Context) -> list[Topic]:
+#     """Get a list of topics from Tome API."""
+#     token = get_bearer_token(context)
     
-    # Call the Tome API to get topics
-    import httpx
-    headers = {
-        "Authorization": f"Bearer {token}", 
-        "Content-Type": "application/json", 
-        "Accept": "application/json",
-    }
+#     # Call the Tome API to get topics
+#     headers = {
+#         "Authorization": f"Bearer {token}", 
+#         "Content-Type": "application/json", 
+#         "Accept": "application/json",
+#     }
     
-    await context.info("Fetching topics from Tome API...")
+#     await context.info("Fetching topics from Tome API...")
     
-    async with httpx.AsyncClient(verify=False) as client:
-        response = await client.get("https://api.dev.toto.nimoto.eu/tometopics/topics", headers=headers)
-        response.raise_for_status()
-        topics_data = response.json()
+#     async with httpx.AsyncClient(verify=False) as client:
+#         response = await client.get("https://api.dev.toto.nimoto.eu/tometopics/topics", headers=headers)
+#         response.raise_for_status()
+#         topics_data = response.json()
         
-        await context.info(topics_data)
+#         await context.info(topics_data)
         
-        return [Topic(topic) for topic in topics_data.get('topics', [])]
+#         return [Topic(topic) for topic in topics_data.get('topics', [])]
 
 
+client = httpx.AsyncClient(base_url="https://api.dev.toto.nimoto.eu/tometopics", verify=False)
+openapi_spec = httpx.get("https://api.dev.toto.nimoto.eu/tometopics/jsondocs", verify=False).json() 
+
+mcp = FastMCP.from_openapi(
+    name="Toto Integration Example",
+    openapi_spec=openapi_spec,
+    client=client,
+    #auth_header_getter=get_bearer_token,
+)
 
 if __name__ == "__main__":
     mcp.run(transport="http", host="127.0.0.1", port=8080, path="/mcp")
